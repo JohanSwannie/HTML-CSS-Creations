c2aa275379f3f6fe900acaa4f2d8cd1cafae5cbe
jswan <jswannieman@gmail.com>
2023-02-14 08:41:05 +1100

    Update progress-bar JavaScript Component

---
+++ Progress-bar/progress-bar.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll("button");
+const rings = document.querySelectorAll(".ring");
+const progressBar = document.querySelector(".indicator");
+
+let latestStep = 1;
+
+const updateSteps = (e) => {
+  latestStep = e.target.id === "next" ? ++latestStep : --latestStep;
+
+  rings.forEach((ring, index) => {
+    ring.classList[`${index < latestStep ? "add" : "remove"}`]("active");
+  });
+
+  progressBar.style.width = `${((latestStep - 1) / (rings.length - 1)) * 100}%`;
+
+  if (latestStep === rings.length) {
+    buttons[1].disabled = true;
+  } else if (latestStep === 1) {
+    buttons[0].disabled = true;
+  } else {
+    buttons.forEach((button) => (button.disabled = false));
+  }
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", updateSteps);
+});
